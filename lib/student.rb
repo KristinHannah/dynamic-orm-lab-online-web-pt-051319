0916444244cf5b796532e03306a830e7885edd8c
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-28 17:59:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student < InteractiveRecord
     self.to_s.downcase.pluralize
   end 
   
-  self.column_names
+  def self.column_names
     DB[:conn].results_as_hash = true 
     
     sql = "PRAGMA table_info('#{table_name}')"
