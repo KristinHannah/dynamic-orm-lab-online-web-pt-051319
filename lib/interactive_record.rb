d0ed76edc6fe6e5903927f4081458a48738e753c
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-28 18:47:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -67,7 +67,7 @@ class InteractiveRecord
       col_name = key.to_s
       value_name = value
     end 
-    sql = "SELECT * FROM #{self.table_name} WHERE #{col_name} = #{value_name};"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{col_name} = '#{value_name}';"
     DB[:conn].execute(sql)
   end 
   
